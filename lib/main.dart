a208f9807b156d867ef1c255e64a4acdfa83d7ef
Tyler Conrad <conradtyler0@gmail.com>
2022-04-08 12:59:42 -0500

    Remove device_preview package from dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart' as m;
-import 'package:flutter/foundation.dart' as f;
-import 'package:device_preview/device_preview.dart' as dp;
 
 import 'package:pavlok_technical_challenge/src/constants.dart' as c;
 import 'package:pavlok_technical_challenge/src/widgets/screen_page_view.dart'
@@ -16,27 +14,17 @@ m.ThemeData _themeData() {
   );
 }
 
-//todo
 void main() {
   m.runApp(
-    dp.DevicePreview(
-      enabled: !f.kReleaseMode,
-      builder: (context) => m.MaterialApp(
-        useInheritedMediaQuery: true,
-        locale: dp.DevicePreview.locale(
-          context,
-        ),
-        builder: dp.DevicePreview.appBuilder,
-        theme: _themeData(),
-        themeMode: m.ThemeMode.light,
-        initialRoute: '/',
-        routes: {
-          '/': (_context) => const m.Scaffold(
-                body: spv.ScreenPageView(),
-              ),
-        },
-      ),
+    m.MaterialApp(
+      theme: _themeData(),
+      themeMode: m.ThemeMode.light,
+      initialRoute: '/',
+      routes: {
+        '/': (_context) => const m.Scaffold(
+              body: spv.ScreenPageView(),
+            ),
+      },
     ),
   );
 }
-//todo
